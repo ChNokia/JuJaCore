6e38a1a909190e774bcb49681f1910a8d2ea8f4e
ChNokia <chizarus@mail.ru>
2015-09-05 17:28:34 +0300

    Update TryWithResource.java
    Correct

--- JuJaCoreWeek6/src/ua/com/juja/core/week6/part1/lab37TryWithResource/TryWithResource.java
+++ JuJaCoreWeek6/src/ua/com/juja/core/week6/part1/lab37TryWithResource/TryWithResource.java
@@ -107,109 +107,63 @@ import java.util.List;
 
  Необходимо реализовать эту логику БЕЗ использования try-with-resources.
  */
+
 public class TryWithResource {
-    public static void main(String[] args) {
-        //prepare
-        final List actualHistoryCall = new ArrayList();
-        final List expectedExceptionTextAndOrder = new ArrayList();
-        expectedExceptionTextAndOrder.add("Error factoryB.createB");
-        expectedExceptionTextAndOrder.add("Error closeA");
-        List expectedHistoryCall = Arrays.asList("factoryA.createA", "factoryB.createB", "A.close");
-
-        final AutoCloseable resourceA = new AutoCloseable() {
-            @Override
-            public void close() throws Exception {
-                actualHistoryCall.add("A.close");
-                throw new Error((String) expectedExceptionTextAndOrder.get(1));
-            }
-        };
-
-        final AutoCloseable resourceB = new AutoCloseable() {
-            @Override
-            public void close() throws Exception {
-                actualHistoryCall.add("B.close");
-            }
-        };
-
-        AutoCloseableFactory factoryA = new AutoCloseableFactory() {
-            @Override
-            public AutoCloseable create() throws Throwable {
-                actualHistoryCall.add("factoryA.createA");
-                return resourceA;
-            }
-
-
-        };
-
-        AutoCloseableFactory factoryB = new AutoCloseableFactory() {
-            @Override
-            public AutoCloseable create() {
-                actualHistoryCall.add("factoryB.createB");
-                throw new Error((String) expectedExceptionTextAndOrder.get(0));
-            }
-
-
-        };
-
-        TryBody tryBody = new TryBody() {
-            @Override
-            public void runBody() {
-                actualHistoryCall.add("TryBody.runBody");
-                //NOP
-            }
-        };
-
-        //call and  check
-        try {
-            TryWithResource.twoResource(factoryA, factoryB, tryBody);
-        } catch (Throwable e) {
-
-            if (!expectedExceptionTextAndOrder.get(0).equals(e.getMessage()))
-                throw new AssertionError("Not correct main exception");
-
-            if (e.getSuppressed().length == 0)
-                throw new AssertionError("Should be suppressed exceptions  ");
-
-            if(!expectedExceptionTextAndOrder.get(1).equals(e.getSuppressed()[0].getMessage()))
-                throw new AssertionError("Not correct suppressed exception should to be "+expectedExceptionTextAndOrder.get(1) +" but found " + e.getSuppressed()[0].getMessage());
-        }
-
-
-        if (!actualHistoryCall.equals(expectedHistoryCall))
-            throw new AssertionError("Not correct order call should be " + expectedHistoryCall.toString() + " but found " + actualHistoryCall.toString());
-
-        System.out.print("OK");
-    }
-
-    public static void twoResource(AutoCloseableFactory factoryA, AutoCloseableFactory factoryB, TryBody body) throws Throwable {
-        AutoCloseable resourceA = factoryA.create();
-        AutoCloseable resourceB = factoryB.create();
+public static void twoResource(AutoCloseableFactory factoryA, AutoCloseableFactory factoryB, TryBody body) throws Throwable {
+    Throwable exeption = new Throwable();
+        boolean isCloseA = false;
+        AutoCloseable resourceA = null;
+        AutoCloseable resourceB = null;
 
         try {
-            body.runBody();
-        } catch (Throwable bodyEx) {
+            resourceA = factoryA.create();
             try {
-                resourceA.close();
-            } catch (Throwable closeExA) {
+                resourceB = factoryB.create();
+                try {
+                    body.runBody();
+                } catch (Throwable bodyEx) {
+                    try {
+                        resourceB.close();
+                    } catch (Throwable closeExB) {
+                        bodyEx.addSuppressed(closeExB);
+                    }
+
+                    try {
+                        isCloseA = true;
+                        
+                        resourceA.close();
+                    } catch (Throwable closeExA) {
+                        bodyEx.addSuppressed(closeExA);
+                    }
+                    throw bodyEx;
+                }
                 try {
                     resourceB.close();
                 } catch (Throwable closeExB) {
-                    bodyEx.addSuppressed(closeExB);
+                    exeption.addSuppressed(closeExB);
                 }
-
-                bodyEx.addSuppressed(closeExA);
-            }
-            try {
-                resourceB.close();
+                try {
+                    isCloseA = true;
+                    
+                    resourceA.close();
+                } catch (Throwable closeExA) {
+                    exeption.addSuppressed(closeExA);
+                }
             } catch (Throwable closeExB) {
-                bodyEx.addSuppressed(closeExB);
+                try {
+                    if ( !isCloseA ) {
+                        resourceA.close();
+                    }
+                } catch (Throwable closeExA) {
+                    exeption.addSuppressed(closeExA);
+                }
+                exeption.addSuppressed(closeExB);
             }
-
-            throw bodyEx;
+        } catch (Throwable closeExA) {
+            if ( !isCloseA ) {
+                exeption.addSuppressed(closeExA);
+            }
         }
-
-        resourceA.close();
-        resourceB.close();
     }
 }
 
@@ -219,4 +173,4 @@ interface AutoCloseableFactory {
 
 interface TryBody {
     public void runBody() throws Throwable;
-}
+}          
