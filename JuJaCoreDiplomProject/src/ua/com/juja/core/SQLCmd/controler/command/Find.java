450e1b6db01a2dbdb58537b2620ca641b569eec8
Sergiy Chyruk <chizarus@mail.ru>
2015-09-22 08:12:35 +0300

    Added command Find

---
+++ JuJaCoreDiplomProject/src/ua/com/juja/core/SQLCmd/controler/command/Find.java
@@ -0,0 +1,62 @@
+package ua.com.juja.core.SQLCmd.controler.command;
+
+import ua.com.juja.core.SQLCmd.model.DataSet;
+import ua.com.juja.core.SQLCmd.model.DatabaseManager;
+import ua.com.juja.core.SQLCmd.view.View;
+
+public class Find implements Command {
+    private DatabaseManager manager;
+    private View view;
+
+    public Find(DatabaseManager manager, View view) {
+        this.manager = manager;
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.startsWith("find|");
+    }
+
+    @Override
+    public void process(String command) {
+        String[] data = command.split("\\|");
+        String tableName = data[1];
+
+        printTable(tableName);
+    }
+
+    private void printTable(String tableName) {
+        printHeader(manager.getTableColumns(tableName));
+        printData(manager.getTableData(tableName));
+    }
+
+    private void printData(DataSet[] tableData) {
+        for (DataSet dataSet : tableData) {
+            printRow(dataSet);
+        }
+    }
+
+    private void printRow(DataSet dataSet) {
+        Object[] values = dataSet.getValues();
+        String result = "|";
+
+        for (Object value : values) {
+            result += value + "|";
+        }
+
+        view.write(result);
+    }
+
+    private void printHeader(String[] tableColumns) {
+        String result = "|";
+
+        for (String columnName : tableColumns) {
+            result += columnName + "|";
+        }
+
+        view.write("--------------------");
+        view.write(result);
+        view.write("--------------------");
+    }
+}
